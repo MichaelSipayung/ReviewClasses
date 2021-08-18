3cf64d25979d3a2bba5694e303c3aa93405c23e3
Michael Sipayung <michaelsipayung123@gmail.com>
2021-08-18 09:48:25 +0700

    Add project files.

---
+++ ReviewClasses/ReviewClasses.cpp
@@ -0,0 +1,37 @@
+﻿// ReviewClasses.cpp : Defines the entry point for the application.
+//
+
+#include "ReviewClasses.hpp"
+int main()
+{
+	std::cout << "Hello classes" << std::endl;
+	return 0;
+}
+double Sales_data::avg_price()const { //const member function 
+	if (unit_sold) {
+		return revenue / unit_sold;
+	}
+	else {
+		return 0;
+	}
+}
+Sales_data& Sales_data::combine(const Sales_data& rhs) {
+	unit_sold += rhs.unit_sold;
+	revenue += rhs.revenue;
+	return *this;
+}
+std::istream& read(std::istream& is, Sales_data& item) {
+	double price = 0;
+	is >> item.bookNo >> item.unit_sold >> price;
+	item.revenue = price * item.unit_sold;
+	return is;
+}
+std::ostream& print(std::ostream& os, const Sales_data& item) {
+	os << item.isbn() << " " << item.unit_sold << " " << item.revenue << " " << item.avg_price();
+	return os;
+}
+Sales_data add(const Sales_data& lhs, const Sales_data& rhs) {
+	Sales_data sum = lhs; //copy data member from lhs into sum 
+	sum.combine(rhs);//add data members from rhs into sum 
+	return sum;
+}
---
+++ ReviewClasses/ReviewClasses.hpp
@@ -0,0 +1,20 @@
+﻿// ReviewClasses.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+// TODO: Reference additional headers your program requires here.
+struct Sales_data
+{
+	std::string isbn()const { return bookNo; }
+	Sales_data& combine(const Sales_data&);
+	double avg_price()const;
+	std::string bookNo;
+	unsigned unit_sold = 0.0;
+	double revenue = 0.0;
+};
+//non member sales_data interface function 
+Sales_data add(const Sales_data&, const Sales_data&);
+std::ostream& print(std::ostream&, const Sales_data&);
+std::istream& read(std::istream&, Sales_data&);
